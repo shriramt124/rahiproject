875c29090587c78d07e56e003224299eb3003159
shriramt124 <shriramt.124@gmail.com>
2024-05-08 13:31:57 +0530

    menue page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
  import Menue from './components/Menue'
 
 import './App.css'
+import { FaShoppingCart } from 'react-icons/fa'
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
   return (
      <div className="app">
 <Menue />
+<FaShoppingCart size={50} className="absolute right-2 bottom-2 bg-red-300 rounded-full p-2 "/>
      </div>
   )
 }
--- client/src/components/Menue.jsx
+++ client/src/components/Menue.jsx
@@ -1,51 +1,48 @@
 import MenuePic from "/nmenuePic.jpeg";
 import "./Menue.css"
+ 
+
+ 
 //function witch create the menu
 //javascript --> html and css
 
 function Menue(){
 return (
-    <div className='wrapper'>
-    <div className="heading">
+    <div className='wrapper relative'>
+    <div className="heading text-center text-5xl my-8 bg-red-300 p-2 uppercase">
      <h1>Menue</h1>
 
     </div>
     <div className="links">
-     <ul>
+     <ul className="flex justify-center gap-6 text-xl bg-red-300 p-2 my-10 ">
        <li>Lunch</li>
        <li>Dinner</li>
        <li>wine list</li>
      </ul>
     </div>
-    <div className="menue-list-wrapper flex">
-     <div className="menue-item flex flex-col sm:flex-row flex-wrap justify-center items-center p-2">
-       <div className="image">
-       <img src={MenuePic} alt="" />
+    <div className="menue-list-wrapper flex flex-wrap w-full items-center gap-2 justify-center bg-green-300 p-2">
+     <div className="menue-item  w-[100%] sm:w-[350px] md:w-[450px] sm:flex-col flex gap-3 bg-red-200 p-2 relative">
+       <div className="image w-[50%] sm:w-[100%] ">
+       <img src={MenuePic} alt=""/>
        </div>
-       <div className="descriptions">
-         <h2>Chicken Parmesan</h2>
-         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, atque!</p>
-       </div>
-     </div>
-     <div className="menue-item flex flex-col sm:flex-row flex-wrap justify-center items-center p-2 ">
-       <div className="image">
-       <img src={MenuePic} alt="" />
-       </div>
-       <div className="descriptions">
-         <h2>Chicken Parmesan</h2>
-         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, atque!</p>
+       <div className="descriptions w-[50%] sm:w-[100%] felx flex-col">
+         <h2 className="text-xl font-semibold">Chicken Parmesan</h2>
+         <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, atque!</p>
+         <div className="order-links flex justify-start gap-4 p-2">
+          <p className="text-xl font-bold">0</p>
+           <button className="text-xl font-bold bg-blue-400 w-[50px] rounded-full">+</button>
+         </div>
+          
        </div>
      </div>
-     <div className="menue-item flex flex-col sm:flex-row flex-wrap justify-center items-center p-2">
-       <div className="image">
-         <img src={MenuePic} alt="" className="w-[200px] " />
-       </div>
-       <div className="descriptions">
-         <h2>Chicken Parmesan</h2>
-         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, atque!</p>
-       </div>
-     </div>
+     
+     
+   
+    
     </div>
+
+    
+     
    </div>
 )
 }
