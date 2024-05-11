351f46f7155afe9291da29de5c75edb70195307f
shriramt124 <shriramt.124@gmail.com>
2024-05-11 12:13:17 +0530

    menue and cart added and app updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,27 @@
 
 import './App.css'
 import { FaShoppingCart } from 'react-icons/fa'
+import {BrowserRouter} from"react-router-dom"
+import { Route,Routes ,Link} from 'react-router-dom'
+import Cart from './components/Cart'
+import { useState } from 'react'
 
 function App() {
  
+
+
  
   return (
      <div className="app">
-<Menue />
-<FaShoppingCart size={50} className="absolute right-2 bottom-2 bg-red-300 rounded-full p-2 "/>
+      <BrowserRouter> 
+      <Routes>
+     <Route path="/" element={<Menue />}/>
+     <Route path="/cart" element ={<Cart />}/> 
+      </Routes>
+      <Link to="/cart"> <FaShoppingCart size={50} className="absolute right-2 bottom-2 bg-red-300 rounded-full p-2 "/>
+ </Link>
+</BrowserRouter>
+ 
      </div>
   )
 }
