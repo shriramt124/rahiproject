bc7206dd46ad1fb62084e3a75bf213e7130c513c
shriramt124 <shriramt.124@gmail.com>
2024-05-12 20:16:34 +0530

    quantity controller is also working now after adding the quantity attrubure in the server

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,35 +1,48 @@
-import { useSelector } from "react-redux"
-import "./Cart.css"
-
- 
+import { useDispatch, useSelector } from "react-redux";
+import "./Cart.css";
+import { descreaseQuantity, inCreaseQuantity } from "../store/cartSlice";
+import { useEffect } from "react";
 
 const Cart = () => {
-  const cartItems = useSelector(state => state.cart.items);
-  console.log(cartItems)
-  if(cartItems.length === 0){
-    return <h1>You cart is empty</h1>
-  }
+  const cartItems = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch()
+  console.log(cartItems);
+  
+ const handleIncQuantity = (id)=>{
+      dispatch(inCreaseQuantity(id))
+ }
+ const handleDecrQuantity = (id)=>{
+ dispatch(descreaseQuantity(id));
+
+ }
+ useEffect(()=>{
+
+ },[cartItems])
+
+ if (cartItems.length === 0) {
+  return <h1>You cart is empty</h1>;
+}
+ 
   return (
     <div className="cart-wrapper">
       <h1 className="cart-heading">Your Items</h1>
-      {
-        cartItems &&  cartItems.map((item)=>(
-          <div className="cart-items " key={item.id*Math.random()*10}>
+      {cartItems &&
+        cartItems.map((item) => (
+          <div className="cart-items " key={item.id * Math.random() * 10}>
             <h4>{item.title}</h4>
-             
+
             <div className="price-item">
-            <h5>{item.price}</h5>
-            <div className="quantity-controller">  
-              <button>-</button>
-              <span>0</span>
-              <button>+</button>
+              <h5>{item.price}</h5>
+              <div className="quantity-controller">
+                <button onClick={() => handleDecrQuantity(item.id)}>-</button>
+                <span>{item.quantity}</span>
+                <button onClick={() => handleIncQuantity(item.id)}>+</button>
               </div>
             </div>
           </div>
-        ))
-      }
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
--- client/src/store/cartSlice.js
+++ client/src/store/cartSlice.js
@@ -7,7 +7,8 @@ const initialState = {
       "title": "How to parse or read",
       "desc": "In this article, I will teach you how to parse or read CSV files in ReactJS  ",
       "image": "https://images.pexels.com/photos/4543099/pexels-photo-4543099.jpeg?auto=compress&cs=tinysrgb&w=600",
-      "price": "1200"
+      "price": "1200",
+      "quantity":"1"
     }]
   };
 
@@ -22,11 +23,30 @@ const cartSlice = createSlice({
         removeItemFromCart(state,action){
              state.items = state.items.filter((item) => item.id !== action.payload);
              
+        },
+        inCreaseQuantity(state,action){
+         
+          //find the item
+          const itemToUpdate = state.items.find((item) => item.id === action.payload);
+          if(itemToUpdate){
+            itemToUpdate.quantity+=1;
+          }
+        },
+        descreaseQuantity(state,action){
+          
+          console.log(action.payload)
+           
+          //find the item
+          console.log('desc run')
+          const itemToudpate = state.items.find((item) => item.id === action.payload);
+          if(itemToudpate && itemToudpate.quantity >0){
+            itemToudpate.quantity-=1;
+          }
         }
     }
 })
 
 
-export const {addToCart,removeItemFromCart} = cartSlice.actions
+export const {addToCart,removeItemFromCart,inCreaseQuantity,descreaseQuantity} = cartSlice.actions
 
 export default cartSlice.reducer;
